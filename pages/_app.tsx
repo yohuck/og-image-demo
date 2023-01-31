9c506693b87d2045a684775da885aeaed5865375
yohuck <ericallenlake@gmail.com>
2023-01-31 15:11:36 -0700

    Image styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Lato } from '@next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Lato({
+  weight: ["100", "300", "900"],
+  subsets: ['latin'] },
+);
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={inter.className} {...pageProps} />
 }
--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -14,12 +14,57 @@ export default function () {
           width: '100%',
           height: '100%',
           display: 'flex',
+          flexDirection: "column",
           textAlign: 'center',
           alignItems: 'center',
-          justifyContent: 'center',
+          justifyContent: 'space-between',
         }}
       >
-        Webinar
+       
+          <h1 style={{
+            fontSize: 128,
+            background: "#0f72b2",
+            padding: "1rem",
+            color: "white",
+            height: "80%",
+            margin: "1rem",
+            fontWeight: "300",
+          }}>Time<span style={{
+            fontWeight: 900,
+            color: "white",
+          }}>Value</span></h1>
+
+
+          <div style={{
+            width: "100%",
+            height: "2px",
+            background: "black",
+          }}></div>
+
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "20%",
+
+          }}>
+            <p style={{
+              fontSize: 32,
+              background: "white",
+            }}>
+              The Industry Leader in Ammortization Software & Financial Calculators since 1984
+            </p>
+
+            <p style={{
+              fontSize: 32,
+              fontWeight: "300",
+
+            }}>
+              Webinar: 2/27/2023 @ 5 PM 
+              </p>
+            </div>
       </div>
     ),
     {
